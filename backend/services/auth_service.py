6e676d8499a0c8319e1ac1da71d16d18532f1c23
nishika931 <sahuunishika@gmail.com>
2026-07-04 23:15:54 +0530

    fixing bug

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -28,10 +28,18 @@ def register_user(db, user):
 
 
 def login_user(db, user):
+    print("Email:", user.email)
+    print("Password length:", len(user.password))
+    print("Password:", user.password)
 
     old_user = db.query(User).filter(User.email == user.email).first()
+    if old_user:
+        print("Stored password hash:", old_user.password)
+        print("Hash length:", len(old_user.password))
+
 
     if not old_user:
+        
         return None
 
     if not verify_password(user.password, old_user.password):
