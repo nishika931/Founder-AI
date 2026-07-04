e007da5e3053c50dd52cb8ba356d4e70550715dd
nishika931 <sahuunishika@gmail.com>
2026-07-04 14:53:03 +0530

    fixing small issue

--- frontend/frontend/src/pages/Dashboard.jsx
+++ frontend/frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/SideBar";
 import MobileNavbar from "../components/MobileNavbar";
 import api from "../services/api";
 
