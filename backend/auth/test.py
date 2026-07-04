2c07b58456d6580721e371e60a868c8d6fad14a3
nishika931 <sahuunishika@gmail.com>
2026-07-04 17:36:33 +0530

    Migrate database from SQLite to PostgreSQL

--- backend/auth/test.py
+++
@@ -1,3 +0,0 @@
-from auth.hash_password import hash_password
-
-print(hash_password("123456"))
--- backend/config/database.py
+++ backend/config/database.py
@@ -1,12 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = "sqlite:///./app.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
